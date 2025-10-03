00385b2f6fd0392764fd72ef1c160b70c9b43f71
milordmario4 <69169723+milordmario4@users.noreply.github.com>
2025-10-03 13:44:12 -0400

    Mutation code source qui passe

--- src/CCoinBox.py
+++ src/CCoinBox.py
@@ -8,13 +8,13 @@ class CCoinBox:
 
     def ajouter_25c(self):
         self.monnaie_courante = self.monnaie_courante + 1
-        if self.monnaie_courante > 1:
+        if self.monnaie_courante >= 1:
             self.vente_permise = True
-        print("Une pièce a été ajoutée")
+        # print("Une pièce a été ajoutée")
 
     def vente(self):
         if self.vente_permise:
-            self.monnaie_totale = self.monnaie_totale + 2
+            # self.monnaie_totale = self.monnaie_totale + 2
             self.monnaie_courante = self.monnaie_courante - 2
             print("Vente! Voici votre article ...")
             if self.monnaie_courante < 2:
@@ -23,7 +23,7 @@ class CCoinBox:
             print("Pas assez de monnaie")
 
     def reset(self):
-        self.monnaie_totale = 0
+        # self.monnaie_totale = 0
         self.monnaie_courante = 0
         self.vente_permise = False
         print("Réinitialisation")
